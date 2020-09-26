00e9a51676c186192d2da0046e344a4e3285a3dd
haonanzhang <haonanzhang415@outlook.com>
2020-09-25 23:33:01 -0700

    upload test file

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -1,5 +1,6 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
@@ -7,35 +8,66 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudStorageApplicationTests {
 
 	@LocalServerPort
 	private int port;
 
-	private WebDriver driver;
+	private static WebDriver driver;
+
+	private String baseURL;
 
 	@BeforeAll
-	static void beforeAll() {
+	public static void beforeAll() {
 		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+	}
+
+	public static void afterAll() {
+		driver.quit();
+		driver = null;
 	}
 
 	@BeforeEach
 	public void beforeEach() {
-		this.driver = new ChromeDriver();
+		baseURL = baseURL = "http://localhost:" + port;
 	}
 
-	@AfterEach
-	public void afterEach() {
-		if (this.driver != null) {
-			driver.quit();
-		}
-	}
+//	@AfterEach
+//	public void afterEach() {
+//		if (this.driver != null) {
+//			driver.quit();
+//		}
+//	}
 
 	@Test
-	public void getLoginPage() {
-		driver.get("http://localhost:" + this.port + "/login");
-		Assertions.assertEquals("Login", driver.getTitle());
+	public void testUserSignupLoginAndSubmitMessage() {
+
+		String username = "pzastoup";
+		String password = "whatabadpassword";
+		String noteTitle = "first note";
+		String noteContent = "this is the content of my note test";
+
+		driver.get(baseURL + "/signup");
+
+		SignupPage signupPage = new SignupPage(driver);
+		signupPage.signup("Peter", "Zastoupil", username, password);
+
+		driver.get(baseURL + "/login");
+
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(username, password);
+
+		HomePage homePage = new HomePage(driver);
+		homePage.createNewNote(noteTitle, noteContent);
+
+		Note createdNote = homePage.getNote();
+
+		assertEquals(noteTitle, createdNote.getNoteTitle());
+		assertEquals(noteContent, createdNote.getNoteDescription());
 	}
 
 }
--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/HomePage.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/HomePage.java
@@ -1,2 +1,49 @@
-package com.udacity.jwdnd.course1.cloudstorage;public class HomePage {
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage {
+
+    @FindBy(id="add-note")
+    private WebElement addNoteButton;
+
+    @FindBy(id="nav-notes-tab")
+    private WebElement noteTab;
+
+    @FindBy(id="note-title")
+    private WebElement noteTitle;
+
+    @FindBy(id="note-description")
+    private WebElement noteContent;
+
+    @FindBy(id="noteSubmit")
+    private WebElement submitButton;
+
+    @FindBy(id="delete-button")
+    private WebElement deleteButton;
+
+    public HomePage(WebDriver driver) { PageFactory.initElements(driver, this); }
+
+    public void createNewNote(String title, String content) {
+        this.noteTab.click();
+        this.addNoteButton.click();
+        this.noteTitle.sendKeys(title);
+        this.noteContent.sendKeys(content);
+        this.submitButton.click();
+    }
+
+    public void deleteNote() {
+        this.deleteButton.click();
+    }
+
+    public Note getNote() {
+        Note note = new Note();
+        note.setNoteTitle(noteTitle.getText());
+        note.setNoteDescription(noteContent.getText());
+        return note;
+    }
 }
